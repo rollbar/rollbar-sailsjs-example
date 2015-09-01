7dd8cc4135d8c10b59285b6f23b87b0ca4ab24c3
Christopher Pfohl <christopher.pfohl@gmail.com>
2015-09-01 10:05:39 -0400

    Make it easy to trigger an error

---
+++ api/controllers/FailuresController.js
@@ -0,0 +1,12 @@
+/**
+ * FailuresController
+ *
+ * @description :: Server-side logic for managing Failures
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	failure: function failure() {
+		throw new Error("This one doesn't work at all");
+	}
+};
